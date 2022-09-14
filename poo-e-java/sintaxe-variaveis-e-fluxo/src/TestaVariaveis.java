883fdb22469f48d35dab50fcf302db95d5e9b8d2
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-14 10:20:25 -0400

    added TestaVariaveis.java

---
+++ poo-e-java/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,18 @@
+import java.net.SocketOption;
+
+public class TestaVariaveis {
+    public static void main(String[] args){
+
+        System.out.println("New Test");
+
+        int age;
+        age = 37;
+
+        System.out.println(age);
+        System.out.println("My age is "+ age + " years old");
+
+        age = age + 10;
+
+        System.out.println("In 2032, i'll have " + age + " years.");
+    }
+}
