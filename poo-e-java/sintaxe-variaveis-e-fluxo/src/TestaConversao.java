fa1d35f3bc4ee1fca80394e009b5c517fe55f32d
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-14 10:53:53 -0400

    added TestaConversao.java

---
+++ poo-e-java/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,8 @@
+public class TestaConversao {
+    public static void main(String[] args){
+        double salary = 1270.50;
+        int value = (int) salary; //casting salary que é double como inteiro
+
+        System.out.println(value);
+    }
+}
