c42cb9789304c14d4d97ecdc22ab078693cd606b
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-15 23:24:58 -0400

    added TesteMultiplos.java

---
+++ poo-e-java/sintaxe-variaveis-e-fluxo/src/TesteMultiplos.java
@@ -0,0 +1,12 @@
+public class TesteMultiplos {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        for(i = 0; i < 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + ". ");
+            }
+        }
+    }
+}
