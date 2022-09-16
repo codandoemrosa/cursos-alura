9e078eb1fb8a7ac8bf8b99ee04dc3d54c7179c06
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-15 22:51:15 -0400

    added TestaWhile.java

---
+++ poo-e-java/sintaxe-variaveis-e-fluxo/src/TestaWhile.java
@@ -0,0 +1,14 @@
+public class TestaWhile {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count <= 10) {
+            System.out.print(count + " ");
+            //count = count + 1;
+            //count =+ 1;
+            count++;
+        }
+    }
+
+}
