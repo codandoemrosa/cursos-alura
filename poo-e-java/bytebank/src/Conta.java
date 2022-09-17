30fd17107f77cde5ebd7603b75e7fbe3fd957b5c
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-17 12:04:03 -0400

    updated *.java files

--- poo-e-java/bytebank/src/Conta.java
+++ poo-e-java/bytebank/src/Conta.java
@@ -15,4 +15,14 @@ public class Conta {
                         return false;
                 }
         }
+        public boolean transfere(double valor, Conta destino){
+                if(this.saldo >= valor){
+                        this.saldo -= valor;
+                        destino.deposita(valor); //mesma coisa de destino.saldo += valor;.
+                        return true;
+                } else {
+                        System.out.println("Você não tem saldo suficiente!");
+                        return false;
+                }
+        }
 }
--- poo-e-java/bytebank/src/TestaMetodo.java
+++ poo-e-java/bytebank/src/TestaMetodo.java
@@ -9,5 +9,19 @@ public class TestaMetodo {
         contaCliente.saca(50);
         System.out.println(contaCliente.saldo);
 
+        Conta contaOutroCliente = new Conta();
+        contaOutroCliente.deposita(1000);
+
+        if (contaOutroCliente.transfere(300, contaCliente)){
+            System.out.println("Transferência ocorrida com sucesso!");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+        System.out.println(contaOutroCliente.saldo);
+        System.out.println(contaCliente.saldo);
+
+        contaCliente.titular = "Erick Leda";
+        System.out.println("O titular da conta é: " + contaCliente.titular);
+
     }
 }
