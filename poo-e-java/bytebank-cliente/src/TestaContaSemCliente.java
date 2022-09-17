c3dd6e54bafd3956550de23497d28a8271a99b65
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-17 13:50:00 -0400

    updated *.java files

--- poo-e-java/bytebank-cliente/src/TestaContaSemCliente.java
+++ poo-e-java/bytebank-cliente/src/TestaContaSemCliente.java
@@ -1,9 +1,7 @@
-import java.sql.SQLOutput;
-
 public class TestaContaSemCliente {
     public static void main(String[] args) {
         Conta contaVazia = new Conta();
-        System.out.println(contaVazia.saldo);
+        System.out.println(contaVazia.getSaldo());
 
         contaVazia.titular = new Cliente();
 
