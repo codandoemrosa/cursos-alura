59fd6e5f85920a7b4b1fdf403ce648a5692c9311
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-17 12:16:41 -0400

    updated *.java files

---
+++ poo-e-java/bytebank-cliente/src/Conta.java
@@ -0,0 +1,28 @@
+public class Conta {
+        double saldo;
+        int agencia;
+        int numeroConta;
+        Cliente titular;
+        public void deposita(double valor){
+                this.saldo += valor; //saldo = saldo + valor;
+        }
+        public boolean saca(double valor){
+                if (this.saldo >= valor){
+                        this.saldo -= valor; //saldo = saldo - valor;
+                        return true;
+                } else {
+                        System.out.println("Você não tem saldo suficiente!");
+                        return false;
+                }
+        }
+        public boolean transfere(double valor, Conta destino){
+                if(this.saldo >= valor){
+                        this.saldo -= valor;
+                        destino.deposita(valor); //mesma coisa de destino.saldo += valor;.
+                        return true;
+                } else {
+                        System.out.println("Você não tem saldo suficiente!");
+                        return false;
+                }
+        }
+}
---
+++ poo-e-java/bytebank-cliente/src/TestaBanco.java
@@ -0,0 +1,16 @@
+public class TestaBanco {
+
+    public static void main(String[] args) {
+        Cliente erick = new Cliente();
+
+        erick.nome ="Erick Leda";
+        erick.cpf ="123.456.789-00";
+        erick.profissao ="Desenvolvedor Fullstack Pleno";
+
+        Conta contaDoErick = new Conta();
+        contaDoErick.deposita(100);
+
+        contaDoErick.titular = erick;
+        System.out.println("O titular dessa conta é " + contaDoErick.titular.nome);
+    }
+}
