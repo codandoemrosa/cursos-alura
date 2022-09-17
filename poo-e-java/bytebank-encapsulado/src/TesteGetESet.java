fe37b34765b47876478f0f06d1cb625c1334fd90
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-17 14:19:54 -0400

    updated *.java files

--- poo-e-java/bytebank-encapsulado/src/TesteGetESet.java
+++ poo-e-java/bytebank-encapsulado/src/TesteGetESet.java
@@ -5,5 +5,20 @@ public class TesteGetESet {
 
         conta.setNumeroConta(1337);
         System.out.println(conta.getNumeroConta());
+
+        Cliente paulo = new Cliente();
+        conta.setTitular(paulo);  //conta.titular = paulo;
+        paulo.setNome("Paulo Silveira"); //conta.nome = "Paulo Silveira";
+
+        System.out.println(conta.getTitular().getNome());
+
+        conta.getTitular().setProfissao("Programador");
+
+        /* mesma coisa de cima, em duas linhas
+
+        Cliente titularDaConta = conta.getTitular();
+        titularDaConta.setProfissao("Programador");
+
+         */
     }
 }
