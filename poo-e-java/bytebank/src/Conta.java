238490c42d51a7bd4a04370dfba85e3ab5bdc8ed
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-17 11:40:43 -0400

    updated Conta.java (added saca())

--- poo-e-java/bytebank/src/Conta.java
+++ poo-e-java/bytebank/src/Conta.java
@@ -6,4 +6,13 @@ public class Conta {
         public void deposita(double valor){
                 this.saldo += valor; //saldo = saldo + valor;
         }
+        public boolean saca(double valor){
+                if (this.saldo >= valor){
+                        this.saldo -= valor; //saldo = saldo - valor;
+                        return true;
+                } else {
+                        System.out.println("Você não tem saldo suficiente!");
+                        return false;
+                }
+        }
 }
