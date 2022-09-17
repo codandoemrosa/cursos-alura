91714e7c12325bb3c9ba339447d1ea68301b9a5c
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-17 11:09:57 -0400

    added TestaMetodo.java

---
+++ poo-e-java/bytebank/src/TestaMetodo.java
@@ -0,0 +1,10 @@
+public class TestaMetodo {
+
+    public static void main(String[] args) {
+        Conta contaCliente = new Conta();
+        contaCliente.saldo = 100;
+        contaCliente.deposita(50);
+
+        System.out.println(contaCliente.saldo);
+    }
+}
